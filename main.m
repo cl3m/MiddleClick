64fe6f28dc567a579f7981d1ee7853bd280ee342
Alex Galonsky <Alex@dy-67-159-68-197.wireless.duke.edu>
2009-11-08 19:20:38 -0500

    Three finger click for middle click with Magic Mouse

--- main.m
+++ main.m
@@ -42,75 +42,28 @@ NSDate *touchStartTime;
 float middleclickX, middleclickY;
 float middleclickX2, middleclickY2;
 MTDeviceRef dev;
+BOOL pressed = NO;
 
 int callback(int device, Finger *data, int nFingers, double timestamp, int frame) {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];	
 
-	//detect triple tap click from raw finger data
-	if (nFingers==0){
-		touchStartTime = NULL;
-		if(middleclickX+middleclickY) {
-			float delta = ABS(middleclickX-middleclickX2)+ABS(middleclickY-middleclickY2); 
-			if (delta < 0.4f) {
-				// Emulate a middle click
-				
-				// get the current pointer location
-				CGEventRef ourEvent = CGEventCreate(NULL);
-				CGPoint ourLoc = CGEventGetLocation(ourEvent);
-				 
-				/*
-				// CMD+Click code
-				CGPostKeyboardEvent( (CGCharCode)0, (CGKeyCode)55, true );
-				CGPostMouseEvent( ourLoc, 1, 1, 1);
-				CGPostMouseEvent( ourLoc, 1, 1, 0);
-				CGPostKeyboardEvent( (CGCharCode)0, (CGKeyCode)55, false );
-				*/
-				
-				// Real middle click
-				CGPostMouseEvent( ourLoc, 1, 3, 0, 0, 1);
-				CGPostMouseEvent( ourLoc, 1, 3, 0, 0, 0);
-				
-			}
+	if(nFingers == 3)
+	{
+		if(!pressed)
+		{
+			CGPostKeyboardEvent( (CGCharCode)0, (CGKeyCode)55, true );
+			pressed = YES;
 		}
-			
-	} else if (nFingers>0 && touchStartTime == NULL){		
-		NSDate *now = [[NSDate alloc] init];
-		touchStartTime = [now retain];
-		[now release];
 		
-		maybeMiddleClick = YES;
-		middleclickX = 0.0f;
-		middleclickY = 0.0f;
-	} else {
-		if (maybeMiddleClick==YES){
-			NSTimeInterval elapsedTime = -[touchStartTime timeIntervalSinceNow];  
-			if (elapsedTime > 0.5f)
-				maybeMiddleClick = NO;
+	}
+	else {
+		if(pressed)
+		{
+			CGPostKeyboardEvent( (CGCharCode)0, (CGKeyCode)55, false );
+			pressed = NO;
 		}
 	}
-	
-	if (nFingers>3) {
-		maybeMiddleClick = NO;
-		middleclickX = 0.0f;
-		middleclickY = 0.0f;
-	}
-	
-	if (nFingers==3) {
-		Finger *f1 = &data[0];
-		Finger *f2 = &data[1];
-		Finger *f3 = &data[2];
-			
-		if (maybeMiddleClick==YES) {
-			middleclickX = (f1->normalized.pos.x+f2->normalized.pos.x+f3->normalized.pos.x);
-			middleclickY = (f1->normalized.pos.y+f2->normalized.pos.y+f3->normalized.pos.y);
-			middleclickX2 = middleclickX;
-			middleclickY2 = middleclickY;
-			maybeMiddleClick=NO;
-		} else {
-			middleclickX2 = (f1->normalized.pos.x+f2->normalized.pos.x+f3->normalized.pos.x);
-			middleclickY2 = (f1->normalized.pos.y+f2->normalized.pos.y+f3->normalized.pos.y);
-		}
-	}
+
 	[pool release];
 	return 0;
 }
